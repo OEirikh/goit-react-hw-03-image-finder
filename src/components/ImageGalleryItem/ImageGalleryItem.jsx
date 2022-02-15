d057c39bdee47fe03df269663064ea67ed734659
Oleksandr Eirikh <o.eyrikh@gmail.com>
2022-02-15 07:58:14 +0200

    structure + css

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,7 @@
+// Описание компонента <ImageGalleryItem>
+// Компонент элемента списка с изображением. Создает DOM-элемент следующей структуры
+// <li class="gallery-item">
+//   <img src="" alt="" />
+// </li>
+
+// import React from "react";
